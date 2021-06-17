54e65b9e71fee69d661a640edd7e681aa00ae3bd
Erik Ayala <ayala.erik61@gmail.com>
2021-06-17 14:49:01 -0500

    pushed this

--- movie.js
+++ movie.js
@@ -1,20 +1,23 @@
+let fetchURL = 'https://calm-sleepy-link.glitch.me/movies'
+
 fetch('https://calm-sleepy-link.glitch.me/movies')
     .then(response => response.json())
     .then(data => {
         console.log(data)
         alert("Loading...")
-    })
+        listMovies(data)
+        })
     .catch(err => {
         console.log(err);
         alert("sorry an error occurred please try again later")
     });
 
 
-$.ajax('https://calm-sleepy-link.glitch.me/movies')
-    .then(response => {
-        console.log(response);
-        listMovies(response);
-        return response;
-    });
+// $.ajax('https://calm-sleepy-link.glitch.me/movies')
+//     .then(response => {
+//         console.log(response);
+//         listMovies(response);
+//         return response;
+//     });
 
 
