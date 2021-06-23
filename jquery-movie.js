2b5405899aa6c0a15de68dc58f38a2e7808afb11
Erik Ayala <ayala.erik61@gmail.com>
2021-06-22 19:55:15 -0500

    final push

--- jquery-movie.js
+++ jquery-movie.js
@@ -1,10 +1,27 @@
-
-function listMovies(moviedata) {
-    console.log(moviedata)
-    let movieList = $('#movieContent')
-    movieList.html("")
-    movieList.append('')
-}
+//
+// function listMovies(moviedata) {
+//     console.log(moviedata)
+//     let movieList = $('#movieContent')
+//     movieList.html("")
+//     movieList.append('')
+// }
 
 // $('#movieContent').append("<p>Hello</p>")
+
+// if (true) {
+//     let x = 5;
+// }
+// console.log(typeof x);
+//
+// let numbers = [1, 2, 3, 4,  5];
+// numbers.map (n => n * 3)
+//
+// console.log(typeof n)
+
+// const isDivisibleBy3 = n => n % 3 === 0;
+//
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//
+// console.log(numbers.filter(isDivisibleBy3));
+//
 
